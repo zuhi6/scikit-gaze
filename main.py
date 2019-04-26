efbcea4a47811a98cb5063a143dc4ef9d1d61c7a
Michal Zajic <zajicmichal6@gmail.com>
2019-04-26 19:38:01 +0100

    moved spam to a different package

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.core.Dataset import Dataset
 from src.common_scanpath.STA import STA
 from src.common_scanpath.eMine import eMine
 from src.common_scanpath.Dotplots import Dotplots
-from src.common_scanpath.SPAM import SPAM
+from src.pattern_search.SPAM import SPAM
 from src.EMDAT.BasicParticipant import BasicParticipant
 from src.core.RQA import RQA
 from src.core.EMDAT import EMDAT
@@ -25,17 +25,18 @@ from src.EMDAT.BasicParticipant import BasicParticipant
 
 
 # emdat = EMDAT(dataset)
-# rqa_test = RQA(dataset)
-# print(rqa_test.CalculateReoccurrenceFunction(5,"ID01"))
+
 
 # dataset = Dataset(pid='P61',eventfile='./data/emdat_test/P61-Event-Data.tsv',dataset_file_path='./data/emdat_test/P61-All-Data.tsv',fixfile='./data/emdat_test/P61-Fixation-Data.tsv',segfile='./data/emdat_test/P61.seg',emdat_aoifile='./data/emdat_test/general.aoi',filtered_dataset=True)
 
 dataset = Dataset('./data/template_sta/scanpaths/DOD2016_fixations_5_participants.tsv',
     './data/template_sta/regions/SegmentedPages.txt',filtered_dataset=False)
+rqa_test = RQA(dataset)
+print(rqa_test.CalculateReoccurrenceFunction(5,"P01m"))
 
-print(eMine(dataset).run_emine())
-part = EMDAT(dataset).createInstance()
-print(part["P01m"].getAoiData("header"))
+# print(eMine(dataset).run_emine())
+# part = EMDAT(dataset).createInstance()
+# print(part["P01m"].getAoiData("header"))
 # # emdat = EMDAT(dataset)
 
 # emdat = BasicParticipant('P61','./data/emdat_test/P61-Event-Data.tsv','./data/emdat_test/P61-All-Data.tsv','./data/emdat_test/P61-Fixation-Data.tsv','./data/emdat_test/P61.seg','./data/emdat_test/general.aoi')
@@ -45,6 +46,5 @@ print(part["P01m"].getAoiData("header"))
 # print(SPAM(dataset).runSpamExtended('./data/spam_test.tsv','./data/spam_result.txt'))
 
 # rqa_test = RQA('./data/rqa_test/ScienceMultipliesEyePowerFixations_Subject01.txt')
-# rqa_test.OpenDataFileFunction()
 # print(rqa_test.CalculateRecurrenceFunction(1,1,1))
 # print(rqa_test.CalculateReoccurrenceFunction(5))
